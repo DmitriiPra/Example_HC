9eb822818504cb92bf100a36480abb1ef96b82b4
Pradim22 <792616@mail.ru>
2023-01-14 15:55:09 +0400

    Hello Masha

---
+++ Example006_Masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя: ");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "маша")
+{
+Console.WriteLine("Да это же Маша!");
+}
+else 
+{
+   Console.Write("Hello, ");   
+   Console.WriteLine(name);
+}
