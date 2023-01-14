268e7b354b2b3a1e9a151cc80ce96e0c76cf26bc
Pradim22 <792616@mail.ru>
2023-01-14 16:54:05 +0400

    Example005 Base

---
+++ Example005_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 4;
+int b = 1;
+int c = 9;
+int d = 6;
+int e = 2;
+
+int max = a;
+if(max < b) max = b;
+if(max < c) max = c;
+if(max < d) max = d;
+if(max < e) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
