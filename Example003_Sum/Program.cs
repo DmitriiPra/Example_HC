037d411e6abd0546acfe977b22aeba5dd2fe913a
Pradim22 <792616@mail.ru>
2023-01-14 15:40:58 +0400

    Exmpl sum random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,7 @@
-﻿int num1 = 2;
-int num2 = 6;
+﻿int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine ("введены числа: ");
+Console.WriteLine (num1);
+Console.WriteLine (num2);
 Console.Write ("сумма чисел равнa = ");
 Console.WriteLine (num1 + num2);
--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,4 @@
-﻿double num1 = 12;
-double num2 = 5;
+﻿double num1 = new Random().Next(1, 10);
+double num2 = new Random().Next(1, 10);
 Console.Write ("результат деления чисел равен = ");
 Console.WriteLine (num1 / num2);
